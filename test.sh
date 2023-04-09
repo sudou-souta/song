83324210da54e2aa60345493f3cee9137ae35358
sudou-souta <126632949+sudou-souta@users.noreply.github.com>
2023-04-09 20:26:43 +0900

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,147 @@
+function c(){
+    cd c
+    bash test.sh
+    cd ..
+}
+function g(){
+    cd g
+    bash test.sh
+    cd ..
+}
+function am(){
+    cd am
+    bash test.sh
+    cd ..
+}
+function f(){
+    cd f
+    bash test.sh
+    cd ..
+}
+function slee(){
+    sleep 0.1
+}
+function online(){
+    # C C
+    c 
+    slee
+    # G G
+    g
+    slee
+    # Am Am
+    am
+    slee
+    # F F
+    f
+    slee
+    # C C
+    c
+    slee
+    # G G
+    g
+    slee
+    # F F
+    f
+    slee
+    # C C
+    c
+    slee
+}
+function online2(){
+    # Am Am
+    am
+    slee
+    # G G
+    g
+    slee
+    # F F
+    f
+    slee
+    # C C
+    c
+    slee
+    # C C
+    c
+    slee
+    # G G
+    g
+    slee
+    # F F
+    f
+    slee
+    # C C
+    c
+    slee
+}
+# C
+function online3(){
+    # F F
+    f
+    slee
+    # C C
+    c
+    slee
+    # G G
+    g
+    slee
+    # C C
+    c
+    slee
+}
+# イントロ
+online
+sleep 0.1
+# Aパート
+online
+sleep 0.1
+online
+sleep 0.1
+# Bパート1
+online2
+sleep 0.1
+# Aパート
+online
+sleep 0.1
+online
+sleep 0.1
+# Bパート
+online2
+sleep 0.1
+online2
+sleep 0.1
+# Cパート
+online3
+slee
+online3
+slee
+# Dパート
+online
+sleep 0.1
+online
+sleep 0.1
+# Bパート
+online2
+sleep 0.1
+online2
+sleep 0.1
+# Aパート
+online
+sleep 0.1
+online
+sleep 0.1
+# Bパート1
+online2
+sleep 0.1
+# Bパート
+online2
+sleep 0.1
+online2
+sleep 0.1
+# Bパート
+online2
+sleep 0.1
+online2
+sleep 0.1
+# C 1/2
+online3
+slee
